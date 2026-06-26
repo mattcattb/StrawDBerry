6ff28a268828d94b5c51b9464a0458a17fb7fe43
Matthew Boughton <mboughton17@gmail.com>
2026-06-26 11:53:52 -0700

    Seperate resp organization

--- resp/reader.go
+++ resp/reader.go
@@ -7,45 +7,8 @@ import (
 	"strconv"
 )
 
-type ValueType byte
-
-const (
-	SimpleString ValueType = '+'
-	SimpleError  ValueType = '-'
-	Integer      ValueType = ':'
-	BulkString   ValueType = '$'
-	BulkError    ValueType = '!'
-	Array        ValueType = '*'
-	Boolean      ValueType = '#'
-	Map          ValueType = '%'
-	NULL         ValueType = '_'
-	Double       ValueType = ','
-	BigNumber    ValueType = '('
-)
-
-type Value struct {
-	typ     ValueType
-	str     string
-	num     int
-	array   []Value
-	boolean bool
-	MAP     [][2]Value
-}
-
 type Resp struct {
 	reader *bufio.Reader
-}
-
-func (v Value) Array() ([]Value, bool) {
-	return v.array, v.typ == Array
-}
-
-func (v Value) BulkString() (string, bool) {
-	return v.str, v.typ == BulkString
-}
-
-func ErrorValue(s string) Value {
-	return Value{typ: SimpleError}
 }
 
 func NewResp(rd io.Reader) *Resp {
